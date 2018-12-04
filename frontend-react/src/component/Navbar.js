6d32e045cd6c5e23f620c257f598637737eb80aa
Pradnyesh <pradnyesh.patil@sjsu.edu>
2018-12-04 01:12:14 -0800

    Fixed Navbar Styling
    Fixed Navbar Styling

--- frontend-react/src/component/Navbar.js
+++ frontend-react/src/component/Navbar.js
@@ -12,34 +12,34 @@ class Navbar extends Component{
         })
     }
 
-    componentDidMount() {
-        document.addEventListener('keydown', function(event) {
-            if(event.keyCode === 13 ) {
-                document.getElementById('searchbutton').click();
-            }
-        });
-    }
-
-    // handleChange = (e) => {
-    //     e.preventDefault();
-    //     this.setState({
-    //         [e.target.name] : e.target.value
+    // componentDidMount() {
+    //     document.addEventListener('keydown', function(event) {
+    //         if(event.keyCode === 13 ) {
+    //             document.getElementById('searchbutton').click();
+    //         }
     //     });
-    //     console.log(this.state);
-    // };
+    // }
 
-    // handleSearch = (e) => {
-    //     e.preventDefault();
-    //     if(this.state.search === '')
-    //         alert("Please enter some search criteria");
-    //     else {
-    //         console.log("Search button clicked");
-    //         let keyword = this.state.search;
-    //         console.log(keyword);
-    //         // this.props.history.push('/filter');
-    //         // Axios Request to database, Splitting and all will be done at backend
-    //     }
-    // };
+    handleChange = (e) => {
+        e.preventDefault();
+        this.setState({
+            [e.target.name] : e.target.value
+        });
+        console.log(this.state);
+    };
+
+    handleSearch = (e) => {
+        e.preventDefault();
+        if(this.state.search === '')
+            alert("Please enter some search criteria");
+        else {
+            console.log("Search button clicked");
+            let keyword = this.state.search;
+            console.log(keyword);
+            // this.props.history.push('/filter');
+            // Axios Request to database, Splitting and all will be done at backend
+        }
+    };
 
     render(){
         const styleForUL = {
@@ -63,34 +63,24 @@ class Navbar extends Component{
         };
         return(
             <ul style={styleForUL}>
+
                 <li style={styleForLi}><a href="/" style = {styleForLiA}>Home</a></li>
                 <li style={styleForLi}><a href="/" style = {styleForLiA}>TV Shows</a></li>
                 <li style={styleForLi}><a href="/" style = {styleForLiA}>Movies</a></li>
                 <li style={styleForLi}><a href="/" style = {styleForLiA}>Recently Added</a></li>
                 <li style={styleForLi}><a href="/" style = {styleForLiA}>My List</a></li>
 
-                {/* <li style={StyleFloatRight}><Link to="/filter" style = {styleForLiA}>Search</Link></li> */}
                 <li style={StyleFloatRight}><Link to="/admin/delete/">Admin Config</Link></li>
                 <li style={StyleFloatRight}><Link to="/signup/">SignUp</Link></li>
                 <li style={StyleFloatRight}><Link to="/signin/">SignIn</Link></li>
                 <li style={StyleFloatRight}><a href="/" style = {styleForLiA}>Account</a></li>
-                <li style={StyleFloatRight}><a id="searchbutton" onClick={this.handleSearch} style = {styleForLiA}>Search</a></li>
-                <li style={StyleFloatRight}>
-                    <div className="form-group">
-                        <input type="text" id='searchbox'
-                               className="form-control"
-                               id="exampleInputTitle"
-                               aria-describedby="emailHelp"
-                               onChange={this.handleChange}
-                               name='search'
-                               placeholder="Search for movie" style={{width:'350px', marginTop : '10px'}} />
-                    </div>
-                </li>
+
                 <li style={StyleFloatRight}>
                     <Link to='/filter'>
                         Search
                     </Link>
                 </li>
+
             </ul>
         )
     }
